fc102850e1eecc8e82cda04586c341126e5b360a
Rajarshi Das <rd@rajarshi.me>
2023-12-29 14:19:47 +0530

    gitignore

--- filesdb/database.sql
+++ filesdb/database.sql
@@ -1,3 +1,6 @@
+create user 'rajarshi'@'localhost';
+set password for 'rajarshi'@'localhost' = password("secret");
+
 create database eleganz_ci4_db;
 grant all on eleganz_ci4_db.* to 'rajarshi'@'localhost';
 flush privileges;
