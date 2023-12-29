ab2132cf9d28fdbcbe432bd4cec9b81e371c2f9f
Rajarshi Das <rd@rajarshi.me>
2023-12-29 15:08:40 +0530

    Setup script updated

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ fx=eleganz.ci4/writable
 
 sudo find eleganz.ci4/ \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
 
+sudo chown -R rd:rd .
+
 sudo chown -R rd:www-data $fx
 sudo chmod -R 775 $fx
 
